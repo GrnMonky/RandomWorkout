535e4d46705f381eeb08470c0a67d422697f1519
Landon <mann.landon@gmail.com>
2014-12-29 15:11:34 -0800

    Added workout ViewController

---
+++ RandomWorkout/Move.swift
@@ -0,0 +1,23 @@
+//
+//  Move.swift
+//  RandomWorkout
+//
+//  Created by Ginkgo on 12/29/14.
+//  Copyright (c) 2014 Ginkgo. All rights reserved.
+//
+
+import Foundation
+
+class Move {
+    
+    var Name:String = ""
+    var Time:Int = 45
+    var Tags:[String] = [String]()
+    
+    class func GenerateMoves() -> [Move]{
+    
+        var moves:[Move] = [Move]()
+        
+        return moves
+    }
+}
