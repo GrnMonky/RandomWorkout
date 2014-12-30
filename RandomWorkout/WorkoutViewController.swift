189da7d0e9cdcf8605cb37ec38869c3d39e253d6
Landon <mann.landon@gmail.com>
2014-12-29 18:09:10 -0800

    added viewController for workout

---
+++ RandomWorkout/WorkoutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WorkoutViewController.swift
+//  RandomWorkout
+//
+//  Created by Ginkgo on 12/29/14.
+//  Copyright (c) 2014 Ginkgo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WorkoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func DoneAction(sender: AnyObject){
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
